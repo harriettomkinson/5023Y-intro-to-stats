73a9dc660305c31a20db62a5149259c39c373cc6
harriettomkinson <qts21qfu@uea.ac.uk>
2023-02-24 12:28:53 +0000

    The Janka data is imported. A simple data check is carried out to check all the column names, checks for duplicates and n/a's in the data.

--- scripts/regression.R
+++ scripts/regression.R
@@ -3,4 +3,32 @@ library(tidyverse)
 library(rstatix)
 library(performance)
 
+# Data Import ----
+janka <- read_csv ("data/Janka.csv")
+#reading the csv file into R
+head(janka)
+#checking data has loaded and first 10 rows printed
+
 #Data Check ----
+colnames(janka)
+#prints the column names
+glimpse(janka)
+#gives number of observations, both numerical and character text
+janka %>%
+  duplicated() %>%
+  sum()
+#checks for duplicate rows in the data - should be zero!
+janka %>%
+  summarise(min=min(dens, na.rm=TRUE),
+            max=max(dens, na.rm=TRUE))
+#gives the minimum and maximum density of the timber
+janka %>%
+  summarise(min=min(hardness, na.rm=TRUE),
+            max=max(hardness, na.rm=TRUE))
+#gives the minimum and maximum hardness of the timber
+janka %>%
+  is.na %>%
+  sum()
+#checks for n/a's in the data frame
+summary(janka)
+#produces a summary of the janka data
